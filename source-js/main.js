096fe4022baec4636f174f419e64269f9b5f65c5
sklyar-angelika <Angelika25051995@gmail.com>
2017-01-28 20:55:51 +0200

    Initial commit

---
+++ source-js/main.js
@@ -0,0 +1,20 @@
+// Can also be used with $(document).ready()
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: true,
+        animationLoop: false,
+        itemWidth: 100,
+        itemMargin: 5,
+        minItems: 2,
+        maxItems: 4
+    });
+});
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+});
+
+
+
